2bd1a144e4682485df046a07bb27d105da763567
Eshban Younis <75836350+eshban14@users.noreply.github.com>
2025-01-13 13:04:30 +0400

    added what i started

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello World! This is the Sandbox Project. And I am started learning c# language!");
     }
 }
